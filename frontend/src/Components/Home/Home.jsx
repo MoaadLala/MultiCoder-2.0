f5d3ec8232d67160fab709eeeabeb5782b0aa686
MoaadLala <moaadlala30@gmail.com>
2021-11-16 20:27:38 +0200

    Working Navbar & Desktop-version Home Page

---
+++ frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,82 @@
+import './Home.css';
+import homeHero from '../../assets/homeHero.png';
+import homePro1 from '../../assets/homePro1.svg';
+import homePro2 from '../../assets/homePro2.svg';
+import homePro3 from '../../assets/homePro3.svg';
+import homePro4 from '../../assets/homePro4.svg';
+import homeGreySecitonImg from '../../assets/homeGreySection.svg';
+
+export default function Home() {
+  return (
+      <div className="home">
+            <div className="homeHero">
+                <div>
+                    <h1>Fight to Earn your Place in a PVP Coding Competition</h1>
+                    <button className="flatBtn">Play</button>
+                </div>
+                <img src={homeHero} alt="" />
+            </div>
+            <div className="homeRedSection">
+                <h1>So, Who Are We?</h1>
+                <p>
+                    MultiCoder is an open-source, newbies-friendly platform where coders from all around the 
+                    world can gather and show off there skills on each other (Not in a weird way), We provide
+                    more than 4 game moods for you, so that you can pick the one that suits you, our goal
+                    is to gamify the process of learning algorithms and data structures (subjects that alot of
+                    coders aren’t very good at ) as much as possible
+                </p>
+            </div>
+            <div className="homeWhyUsSection">
+                <h1>Why Us?</h1>
+                <div className="homeProsSection">
+                    <div className="homePro">
+                        <img src={homePro1} alt=""/>
+                        <h3>Compete against your friends</h3>
+                        <p>
+                            Am sure that you are eager to prove that your
+                            code is better than your co-worker John’s code,
+                            or maybe all of your co-workers? doesn’t matter
+                            alone, with a friend, or a bunch of friends! totally
+                            your choice
+                        </p>
+                    </div>
+                    <div className="homePro">
+                        <img src={homePro2} alt=""/>
+                        <h3>From the Comunity to The Comunity</h3>
+                        <p>
+                            MultiCoder Is an open source project, where
+                            anyone could view, and suggest changes to
+                            the code, also, anyone could add questions,
+                            and provide better solutions to already existing
+                            ones
+                        </p>
+                    </div>
+                    <div className="homePro">
+                        <img src={homePro3} alt=""/>
+                        <h3>Everything is In-House</h3>
+                        <p>
+                            Comes with an Editor, Compiler, and everthing
+                            you need to win your battles
+                        </p>
+                    </div>
+                    <div className="homePro">
+                        <img src={homePro4} alt=""/>
+                        <h3>Beautifully Designed Rating System</h3>
+                        <p>
+                            We Make a great effort to make sure that you
+                            don’t feel frustrated, or (which i doubt it) bored
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="homeGreySection">
+                <div>
+                    <h1>Learn, Compete, and Win Prizes!</h1>
+                    <p>MultiCoder hosts competitions that are provided by you! the users, so you are more than welcome to compete in competitions, or create your own ones!</p>
+                    <button className="flatBtn">Find Out More</button>
+                </div>
+                <img src={homeGreySecitonImg} alt="" />
+            </div>
+      </div>
+  )
+}
---
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { useState } from 'react';
+
+export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const changeMenuState = () => {
+        const hamBtn = document.querySelector('.hamBtn');
+        const mobileMenu = document.querySelector('.mobileMenu');
+        const mobileNav = document.querySelector('.mobileNav');
+        if (isMenuOpen) {
+            hamBtn.innerHTML = '<i class="fas fa-bars"></i>';
+            mobileMenu.style.display = 'none';
+            setIsMenuOpen(false);
+            mobileNav.style.boxShadow = 'rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset';
+        } else {
+            hamBtn.innerHTML = '<i class="fas fa-times"></i>';
+            mobileMenu.style.display = 'block';
+            mobileNav.style.boxShadow = 'none';
+            setIsMenuOpen(true);
+        }
+    }
+
+    return (
+        <>
+            <nav>
+                <div className="leftSide">
+                    <h3 className="navBrand">Multi<span>Coder</span></h3>
+                    <Link to="/">Home</Link>
+                    <Link to="/">About</Link>
+                    <Link to="/">Play</Link>
+                </div>
+                <div className="rightSide">
+                    <button class="flatBtn">Sign Up</button>
+                </div>
+                <div className="mobileNav">
+                    <h3 className="navBrand">Multi<span>Coder</span></h3>
+                    <button className="hamBtn" onClick={changeMenuState}><i class="fas fa-bars"></i></button>
+                </div>
+            </nav>
+            <div className="mobileMenu">
+                <div className="mobileMenuItem">Home</div>
+                <div className="mobileMenuItem">About</div>
+                <div className="mobileMenuItem">Play</div>
+                <button class="flatBtn">Sign Up</button>
+            </div>
+        </>
+    )
+}
